39e1fc88d2d32c92b728267be8b2a951be1dca37
JOSHY7002 <151787243+JOSHY7002@users.noreply.github.com>
2023-12-22 13:17:29 -0800

    Add files via upload

--- setup/app.js
+++ setup/app.js
@@ -1,15 +1,49 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+const randt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
+let random = 0;
+  for (let i = 0; i < 1; i++) {
+    random += randt[getRandomNumber()];
+  }
 
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    }  else if (styles.contains("plusTen")){
+        count += 10;
+    }  else if (styles.contains("minusTen")){
+        count -= 10;
+    }
+    else if (styles.contains("random")){
+        count += random;
+    }
+    else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
-}
+    return Math.floor(Math.random() * randt.length);
+  }
+ 
